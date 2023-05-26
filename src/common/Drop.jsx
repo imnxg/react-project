8ac727b7242b706b1d5149d3ecc05301780f6359
imnxg <hisnxg@qq.com>
2023-05-26 09:48:09 +0800

    update 设置、退出

--- src/common/Drop.jsx
+++ src/common/Drop.jsx
@@ -2,9 +2,9 @@ import { Dropdown, Menu, Button, Space } from '@arco-design/web-react';
 import { IconDown } from '@arco-design/web-react/icon';
 const dropList = (
   <Menu>
-    <Menu.Item key='1'>Beijing</Menu.Item>
-    <Menu.Item key='2'>Shanghai</Menu.Item>
-    <Menu.Item key='3'>Guangzhou</Menu.Item>
+    <Menu.Item key='1'>设置</Menu.Item>
+    <Menu.Item key='2'>个人中心</Menu.Item>
+    <Menu.Item key='3'>退出</Menu.Item>
   </Menu>
 );
 
